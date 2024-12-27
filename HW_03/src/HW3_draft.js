bb5b6b434345f2e820d91c010df746d32219750b
Ihor Kumanov <kumdmitrij@gmail.com>
2024-12-27 23:41:42 +0200

    add HW_03

---
+++ HW_03/src/HW3_draft.js
@@ -0,0 +1,162 @@
+// const - нельзя перевизначити
+// let - можна
+// {
+//     /*
+//     console.log(2);
+//     console.log(1);
+//     */
+// }
+
+// let a = 5;
+// a = 10
+// console.log(a)
+
+// const b = 100;
+// b = 15;
+
+// console.log(b);
+
+// const x = 101
+
+// console.log(x)
+
+// let y = 101
+// console.log(y)
+
+// const x = 10000
+// console.log(x)
+
+// const a = 10
+// const b = 10
+// const c = 10
+// Або
+// const a = b = c = 10
+
+// let x = y = p = 5
+
+// x = 10
+
+// console.log(x)
+// console.log(y)
+
+
+// let randomNumber = 1000
+// console.log(randomNumber)
+
+// const message = 'Hi am massage'
+// console.log(message)
+
+// const newMessage = 'Hi world'
+// console.log(newMessage)
+
+// const змінна = 123 - так робити не потрібно
+// const 123hello = 2 - не можна починати з цифри, тільки в кінці
+// const mar)~k = 2 - неможна використовувати символи, крім "_"
+// const let = 2 - не можна використовувати службові слова
+
+// const down -
+// const DOWN - це буде різні змінні
+
+// const title = 'How to name variables'
+// const category = 'JavaScript'
+// const formError = 'This field is required'
+// const isFriday = true
+
+
+// {
+//     var nerVariable = 17 - var видно за межами блоку коду
+// }
+// console.log(nerVariable)
+
+// const hostingVar = 177
+// console.log(hostingVar)
+
+
+// const newVariable = 'Some Text'
+// var anotherVariable = 'Another text'
+// console.log(newVariable, anotherVariable) - можна віводити делілька змінних
+
+/* !! Condition
+
+const lessZeroMessage = 'Число меньше нуля'
+const biggerZeroMessage = 'Число більше нуля'
+const someNomber = 2
+
+if (someNomber > 0) { //true
+    console.log(biggerZeroMessage)
+}
+else {
+    console.log(lessZeroMessage)
+}
+*/
+
+/*
+const lessZeroMessage = 'Число меньше нуля'
+const biggerZeroMessage = 'Число більше нуля'
+const biggerHundred = 'Число більше 100'
+const lessHundredBiggerZerro = 'Число більше нуля але меньше 100'
+const someNomber = 115
+
+if (someNomber > 0) 
+{
+    console.log(biggerZeroMessage)
+    if (someNomber > 100) 
+    {
+        console.log(biggerHundred)
+    }
+    else 
+    {
+        console.log(lessHundredBiggerZerro)
+    }
+}
+else 
+{
+    console.log(lessZeroMessage)
+}
+*/
+
+// let testVar = 15;
+
+// if (testVar > 0)
+// {
+//     console.log(testVar)
+// }
+
+// const newVar = prompt ('Введіть число')
+// console.log(newVar)
+
+//!!!! prompt, if, alse, змінні - що потрібно для домашки
+
+// const currentTemperature = +prompt ('Скільки градусів') // + чтоб получить число
+
+// if (Number.isNaN(currentTemperature)) {
+//     console.log("Невірна температура")
+// } else {
+
+// if (currentTemperature > 0) {
+//     console.log('На вулиці тепло', currentTemperature)
+
+//     } else {
+//         console.log ('На вулиці холодно', currentTemperature)
+//     }
+// }
+
+
+/*!! ТЕРНАРНИЙ ОПЕРАТОР
+const currentTemperature = +prompt ('Скільки градусів') // + чтоб получить число
+const message = 
+    currentTemperature >= 0 ? 'на вулиці тепло: ' : 'на вулиці холодно: '
+
+console.log(message, currentTemperature)
+*/
+
+/* Домашка
+const N = 10
+if (N % 3 === 0) {
+
+}
+
+if (N % 3 === 0 && N % 5 === 0) {
+
+}
+*/
