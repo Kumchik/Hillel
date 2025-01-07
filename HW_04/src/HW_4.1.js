a2d97b71e4c8d205ad9289d590e9d247e08bab4d
Ihor Kumanov <kumdmitrij@gmail.com>
2025-01-07 22:05:32 +0200

    add 4

--- HW_04/src/HW_4.1.js
+++ HW_04/src/HW_4.1.js
@@ -1,9 +1,25 @@
-const N = +prompt ('Введіть число')
+const num = +prompt ('Enter num: ')
 
-if (isNaN(N) || N <= 1) {
-    console.log('Введіть коректне значення')
-} else if (N % 2 !== 0 && N % N === 0 || N === 2) {
-    console.log(`Число ${N} є простим числом`)
-} else {
-    console.log(`Число ${N} не є простим числом`)
+for (let i = 2; i <= num; i++) {
+    // console.log (num, '-', i, '-', num % i);
+
+    if (num % i === 0) {
+        console.log ('Просте');
+        break;
+    }
+
+    console.log (i);
 }
+
+
+
+
+
+
+// if (isNaN(N) || N <= 1) {
+//     console.log('Введіть коректне значення')
+// } else if (N % 2 !== 0 && N % N === 0 || N === 2) {
+//     console.log(`Число ${N} є простим числом`)
+// } else {
+//     console.log(`Число ${N} не є простим числом`)
+// }
