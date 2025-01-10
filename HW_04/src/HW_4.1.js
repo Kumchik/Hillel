676bc474808b77fa24427ff5c0c6ccdf1cec9b49
Ihor Kumanov <kumdmitrij@gmail.com>
2025-01-10 14:36:41 +0200

    change 4.1

--- HW_04/src/HW_4.1.js
+++ HW_04/src/HW_4.1.js
@@ -1,15 +1,19 @@
 let num = +prompt('Введіть число');
-
 let sum = true;
 
 for (let i = 2; i < num; i++) {
+
+
     if (num % i === 0) {
         sum = false;
         break;
-    }
+    } 
 }
 
-if (sum) {
+if (isNaN(num) || num <= 0){
+    console.log('Введіть коректне значення')
+}
+else if (sum) {
     console.log('Просте число.');
 } else {
     console.log('Складене число.');
