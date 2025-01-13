8d25cfa3b61490218247c32cabadb91eca4b82b3
Ihor Kumanov <kumdmitrij@gmail.com>
2025-01-13 16:50:45 +0200

    add 6.2

---
+++ HW_06/src/HW_6.2.js
@@ -0,0 +1,10 @@
+const input = prompt ('Введіть значення');
+
+function padString (str, length, symbol, toLeft = false) {
+    if (str.length <= length) {
+        return str;
+    }
+    
+}
+
+console.log(padString(input));
