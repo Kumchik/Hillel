e11fcab5f6330677bb3c854141e20700188f4a39
Ihor Kumanov <kumdmitrij@gmail.com>
2025-02-25 19:17:10 +0200

    add 16

---
+++ HW_16/HW16_draft.js
@@ -0,0 +1,34 @@
+/*
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 42
+};
+
+
+Object.freeze(person);
+Object.seal(person);
+
+person.newProp = 'New Prop';
+person.age = 98;
+console.log(person.age);
+*/
+
+
+
+/*
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 42
+};
+
+
+console.log(Object.getOwnPropertyDescriptors(person));
+console.log(Object.getOwnPropertyDescriptor(person, 'firstName'));
+
+
+person.newProp = 111;
+
+console.log(Object.getOwnPropertyDescriptor(person, 'newProp'));
+*/
